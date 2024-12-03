436495cfad74a1c23e6008c54ae3be820baed0cd
Arturo <soibcursos@gmail.com>
2024-12-03 14:54:52 +0100

    2024-12-03 arrays y funciones

--- 09-funciones/10-funciones.js
+++ 09-funciones/10-funciones.js
@@ -1,7 +1,7 @@
 //Escribe una función que pase de euros a dolares
 //Un Euro son 1.05 USD
-
-function pasaElBillete(eurete){
     eurete=prompt("Cuántos euretes tienes en el bolsillete? ");
+function pasaElBillete(eurete){
+
     let cambio=eurete*1.05;
 }
