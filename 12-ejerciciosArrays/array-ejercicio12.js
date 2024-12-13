90e692a7b1b76d70cfbe05a9b4c306254b2e4c8c
Arturo <soibcursos@gmail.com>
2024-12-13 14:37:13 +0100

    2024-12-13_Fin-clase

--- 12-ejerciciosArrays/array-ejercicio12.js
+++ 12-ejerciciosArrays/array-ejercicio12.js
@@ -1,4 +1,6 @@
 //ejercicios arrays
+
+//Lo siguiente es un array -[]- de objetos -{}- 
 const empresas = [
     { name: "Financiera Alpha", category: "Finanzas", start: 1981, end: 2004 },
     { name: "Tienda Beta", category: "Minorista", start: 1992, end: 2008 },
@@ -16,4 +18,37 @@ const empresas = [
     });
 console.log(newEmpresas);
 // Obrtener las empresas que tienen  10 años o mas
+const empresasMas10=empresas.filter((todosElementos)=>{
+  return todosElementos.end - todosElementos.start >=10;
+  });
+  console.log("Empresas de más de 10 años", empresasMas10);
 
+  //map es igual que el foreach pero devuelve un array nuevo
+//con lo que devuelva el callback
+const empresasYearsOfActivity=empresas.map((todosElementos)=>{
+  const activity={
+    name: todosElementos.name,
+    years: todosElementos.end - todosElementos.start,
+  };
+  return activity;
+});
+
+/*Se pide un array nuevo solo con el nombre de las empresas en mayusculas*/
+
+const empresasMayusculas=empresas.map((todosElementos)=>{
+  const mayusculas={
+    name:todosElementos.name.toUpperCase()
+  }
+  return mayusculas;
+})
+console.log(empresasMayusculas);
+
+//imprimir con document.write lo siguiente para cada empresa:
+//<p>Finanzas <strong>'Financiera Alpha'</strong> te desea <em> Feliz Navidad</em>.</p>
+//<sm>Contigo desde 1981. </sm> (con forEach)
+
+empresas.forEach((todosElementos)=>{
+  const text=`<p>${todosElementos} <strong>${todosElementos.name}</strong> te desea <em> Feliz Navidad</em>.</p>
+  <sm>Contigo desde ${todosElementos.start}. </sm>`;
+  document.write(text);
+});
--- 13-ArrayMethods/09.4-funciones.js
+++ 13-ArrayMethods/09.4-funciones.js
@@ -1,1 +1,33 @@
-//Mas sobre callBacks
+//Mas sobre callBacks funciones con objetos
+//los objetos seran recaChipirones y recetaPatatas
+
+const recetaChipirones = {
+    nombre: "Chipirones al pil-pil",
+    ingredientes: "chipirones",
+    tiempoCoccion: 5000,
+  };
+  const recetaPatatas = {
+    nombre: "Patatas Fritas",
+    ingredientes: "patatas",
+    tiempoCoccion: 8000,
+  };
+
+  function cocinar(receta, callback) {
+    console.log(`Oido cocina: una de ${receta.nombre}...`);
+    console.log(`Preparando ingredientes ${receta.ingredientes}`);
+    setTimeout(() => {
+      const plato = `Una ración de ${receta.nombre}`;
+      callback(plato);
+    }, receta.tiempoCoccion);
+  }
+
+  function servirPlato(plato) {
+    console.log(`!Sirviendo el ${plato} en la mesa 10!`);
+  }
+
+  function paraLlevar(plato) {
+    console.log(`!El ${plato} listo para Glovo`);
+  }
+  console.log("Iniciando pedidos...");
+  cocinar(recetaChipirones, servirPlato);
+  cocinar(recetaPatatas, paraLlevar);
